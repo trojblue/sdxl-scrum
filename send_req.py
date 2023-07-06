09ed89a9faa0e97362c351258136c1fec9011769
yada <trojblue@gmail.com>
2023-07-06 08:32:32 -0400

    feat: adding text reader & gradio ui

--- send_req.py
+++ send_req.py
@@ -1,12 +1,24 @@
 import requests
 import time
 import json
+from tqdm import tqdm
+from pathlib import Path
+from unibox import UniLoader, UniLogger
+import gradio as gr
+
+logger = UniLogger("_data", file_suffix="sdxl_scrum")
+data_loader = UniLoader(logger)
+
+def load_txt(txt_path):
+    return data_loader.load_txt(Path(txt_path))
 
 def send_post_request(endpoint, params):
     try:
         response = requests.post(endpoint, data=json.dumps(params), headers={'Content-Type': 'application/json'})
         response.raise_for_status()  # Will raise exception if status is 4xx, 5xx
-        return response.json()
+        json_response = response.json()
+        print(json_response)  # print the response
+        return json_response
     except requests.exceptions.HTTPError as errh:
         print("Http Error:", errh)
     except requests.exceptions.ConnectionError as errc:
@@ -16,14 +28,31 @@ def send_post_request(endpoint, params):
     except requests.exceptions.RequestException as err:
         print("Something went wrong", err)
 
+
 def send_post_requests_at_rate(endpoint, params_list, qps):
     interval = 1.0 / qps if qps != 0 else 0
-    for params in params_list:
+    for params in tqdm(params_list):  # Add tqdm for progress bar
         send_post_request(endpoint, params)
         time.sleep(interval)
 
+def send_requests_from_ui(input_urls):
+    endpoint = 'https://etewv9yzp0.execute-api.us-west-2.amazonaws.com/Prod/gdl-api-gateway-to-sqs'
+    params_list = [{'command': url} for url in input_urls.split('\n')]
+    send_post_requests_at_rate(endpoint, params_list, 2)
+
+
+def _send_single_req():
+    endpoint = 'https://etewv9yzp0.execute-api.us-west-2.amazonaws.com/Prod/gdl-api-gateway-to-sqs'
+    params_list = [
+        {'command': "\"https://www.artstation.com/sleeping_bird\"",
+         "s3_uri": "s3://dataset-ingested/gallery-dl/"
+         }
+    ]
+    send_post_requests_at_rate(endpoint, params_list, 2)
+
 
 if __name__ == '__main__':
-    endpoint = 'http://localhost:5000/echo'
-    params_list = [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'Charlie'}]
-    send_post_requests_at_rate(endpoint, params_list, 2)
+    # Gradio UI
+    iface = gr.Interface(fn=send_requests_from_ui, inputs='text', outputs='text')
+    iface.launch()
+
